82b5d3f07ec8ff6f7e3ed7c882cb0ff3b7df1108
Sukianto Suteja <42060507+sukibeww@users.noreply.github.com>
2019-10-22 15:20:44 +1100

    created login form components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar'
+import Login from './components/login/Login'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Login />
     </div>
   );
 }
